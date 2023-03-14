509aff29901086acfff2894963132f020c43ab64
Kure-ru <clairedochez@gmail.com>
2023-03-14 17:34:25 +0900

    implemented dark mode

--- src/App.js
+++ src/App.js
@@ -1,30 +1,28 @@
 import { useState } from "react";
-import iconMoon from "./assets/icon-moon.svg";
 
 import { TodoInput } from "./components/TodoInput";
 import { TasksDisplay } from "./components/TasksDisplay";
 import { TaskBar } from "./components/TaskBar";
+import { DarkMode } from "./components/DarkMode";
 
-import "./App.css";
+import "./DarkMode.css";
 
 function App() {
+
   const [tasks, setTasks] = useState([]);
   const [mode, setMode] = useState("all");
 
-  console.log(mode);
   return (
     <>
       <section class="app__header">
         <div class="header__top">
           <h1 class="top__title">TODO</h1>
-          <div class="top__icon">
-            <img src={iconMoon} alt="moon indicating night mode button" />
-          </div>
+          <DarkMode/>
         </div>
         <TodoInput tasks={tasks} setTasks={setTasks} />
       </section>
 
-      <TasksDisplay tasks={tasks} setTasks={setTasks} />
+      <TasksDisplay tasks={tasks} setTasks={setTasks} mode={mode} />
 
       <TaskBar
         tasks={tasks}
