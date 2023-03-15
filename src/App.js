84337ad38b99f0e45ce4a41f948089228f377cae
Kure-ru <clairedochez@gmail.com>
2023-03-15 19:20:56 +0900

    refactored css

--- src/App.js
+++ src/App.js
@@ -8,30 +8,30 @@ import { DarkMode } from "./components/DarkMode";
 import "./DarkMode.css";
 
 function App() {
-
   const [tasks, setTasks] = useState([]);
   const [mode, setMode] = useState("all");
 
   return (
     <>
-    <div className="background__image"></div>
+      <div className="background__image"></div>
       <section class="app__header">
         <div class="header__top">
           <h1 class="top__title">TODO</h1>
-          <DarkMode/>
+          <DarkMode />
         </div>
-        <TodoInput tasks={tasks} setTasks={setTasks} />
       </section>
+      <div className="app__wrapper">
+        <TodoInput tasks={tasks} setTasks={setTasks} />
 
-      <TasksDisplay tasks={tasks} setTasks={setTasks} mode={mode} />
+        <TasksDisplay tasks={tasks} setTasks={setTasks} mode={mode} />
 
-      <TaskBar
-        tasks={tasks}
-        setTasks={setTasks}
-        mode={mode}
-        setMode={setMode}
-      />
-
+        <TaskBar
+          tasks={tasks}
+          setTasks={setTasks}
+          mode={mode}
+          setMode={setMode}
+        />
+      </div>
       <p className="app__bottom__text">Drag and drop to reorder list</p>
     </>
   );
